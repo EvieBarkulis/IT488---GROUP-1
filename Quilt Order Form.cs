10bd35262ac4bd6157abbed7effde9763c55f177
JBrown2288 <106568688+JBrown2288@users.noreply.github.com>
2022-10-07 18:50:54 -0500

    Add files via upload
    Signed-off-by: JBrown2288 <106568688+JBrown2288@users.noreply.github.com>

--- Quilt Order Form.cs
+++ Quilt Order Form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace IT488_Group1_Project
 {
@@ -15,6 +16,30 @@ namespace IT488_Group1_Project
         public Quilt_Order_Form()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Guid myNewId = Guid.NewGuid();
+
+            SqlConnection con = new SqlConnection("Integrated security=true; Initial Catalog=MWC_CustomOrder; Data source = MJ0E1DP0\\MSSQLSERVER01");
+            SqlCommand cmd;
+            con.Open();
+            string s = "insert into quiltOrders values (@quiltOrderID, @email, @quiltOrderType, @quiltOrderSize, @quiltOrderFabric, @quiltOrderDesign, @quiltOrderFiller, @quiltOrderBacking)";
+            cmd = new SqlCommand(s, con);
+            cmd.Parameters.AddWithValue("@quiltOrderID", myNewId);
+            cmd.Parameters.AddWithValue("@email", textBox_email.Text);
+            cmd.Parameters.AddWithValue("@quiltOrderType", comboBox_type.Text);
+            cmd.Parameters.AddWithValue("@quiltOrderSize", comboBox_size.Text);
+            cmd.Parameters.AddWithValue("@quiltOrderFabric", comboBox_fabric.Text);
+            cmd.Parameters.AddWithValue("@quiltOrderDesign", comboBox_design.Text);
+            cmd.Parameters.AddWithValue("@quiltOrderFiller", comboBox_filler.Text);
+            cmd.Parameters.AddWithValue("@quiltOrderBacking", comboBox_backing.Text);
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Your Order has been submitted! We will email you shortly with a confirmation number.");
+            this.Close();
         }
     }
 }
